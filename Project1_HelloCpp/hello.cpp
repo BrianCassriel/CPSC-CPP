14ce4975aff2a66dc1bc6c36cd3b44e453bd5cf3
BrianCassriel <112517291+BrianCassriel@users.noreply.github.com>
2023-02-08 20:01:48 -0800

    add hello.cpp

---
+++ Project1_HelloCpp/hello.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  Project1_HelloCpp
+//
+//  Created by Brian Cassriel on 2/8/23.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello C++!" << std::endl;
+    return 0;
+}
