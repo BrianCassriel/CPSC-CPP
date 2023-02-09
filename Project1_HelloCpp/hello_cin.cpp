9f146c67dfb7a080dd4362530cbf0e3861db3f94
BrianCassriel <112517291+BrianCassriel@users.noreply.github.com>
2023-02-08 20:06:15 -0800

    add hello_cin.cpp

---
+++ Project1_HelloCpp/hello_cin.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project1_HelloCpp
+//
+//  Created by Brian Cassriel on 2/8/23.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   string strMessage;
+   cout << "Please type a single word message: " << endl;
+   cin >> strMessage;
+   cout << "Message: " << strMessage << endl;
+   return 0;
+}
