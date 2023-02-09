f592d79ebd3eb6822cd1bc210f0c655210849ced
BrianCassriel <112517291+BrianCassriel@users.noreply.github.com>
2023-02-08 20:04:13 -0800

    add hello_namespace.cpp

---
+++ Project1_HelloCpp/hello_namespace.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Project1_HelloCpp
+//
+//  Created by Brian Cassriel on 2/8/23.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello C++!" << std::endl;
+    return 0;
+}
